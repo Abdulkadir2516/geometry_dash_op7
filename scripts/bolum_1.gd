eda30a7e565738e79cf3a365a5365e85bafc9c4a
Abdulkadir Yeşilkaya <101483379+Abdulkadir2516@users.noreply.github.com>
2025-07-05 21:10:57 +0300

    .

--- scripts/bolum_1.gd
+++ scripts/bolum_1.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @export var scroll_speed: float = 80.0  # Piksel/saniye
+
 var bg1: Sprite2D
 var bg2: Sprite2D
 var bg_width: float
@@ -10,33 +11,40 @@ var g2: Sprite2D
 
 func _ready():
 	bg1 = $"Bg03-hd"
-	bg2 = $"Bg03-hd2"
-	bg_width = bg1.texture.get_width()
-
-	# İkinci arka planı hemen birincinin sağına koy
-	bg2.position.x = bg1.position.x + 1170
-	
+	bg2 = $"Bg03-hd2"	
 	g1 = $"Ground"
 	g2 = $"Ground2"
-
-	# İkinci arka planı hemen birincinin sağına koy
-	g2.position.x = g1.position.x + 1170
-
-func _process(delta):
-	bg1.position.x -= scroll_speed * delta
-	bg2.position.x -= scroll_speed * delta
-
-	g1.position.x -= scroll_speed * delta
-	g2.position.x -= scroll_speed * delta
 	
-	# Eğer bir arka plan tamamen ekranın soluna geçtiyse en sağa taşı
-	if bg1.position.x <= -600:
-		bg1.position.x = bg2.position.x + 1170
-	elif bg2.position.x <= -600:
-		bg2.position.x = bg1.position.x + 1170
+	doit(bg1,80)
+	doit(bg2,80)
+	doit(g1,100)
+	doit(g2,100)
 
-	# Eğer bir arka plan tamamen ekranın soluna geçtiyse en sağa taşı
-	if g1.position.x <= -600:
-		g1.position.x = g2.position.x + 1170
-	elif g2.position.x <= -600:
-		g2.position.x = g1.position.x + 1170
+#func _process(delta):
+	#bg1.position.x -= scroll_speed * delta
+	#bg2.position.x -= scroll_speed * delta
+#
+	#g1.position.x -= scroll_speed * delta *3
+	#g2.position.x -= scroll_speed * delta *3
+	#
+	## Eğer bir arka plan tamamen ekranın soluna geçtiyse en sağa taşı
+	#if bg1.position.x <= -600:
+		#bg1.position.x = bg2.position.x + 1170
+	#elif bg2.position.x <= -600:
+		#bg2.position.x = bg1.position.x + 1170
+#
+	## Eğer bir arka plan tamamen ekranın soluna geçtiyse en sağa taşı
+	#if g1.position.x <= -600:
+		#g1.position.x = g2.position.x + 1170
+	#elif g2.position.x <= -600:
+		#g2.position.x = g1.position.x + 1170
+
+
+func doit(nesne: Sprite2D, hiz:int):
+	
+	while true:
+		nesne.position.x += hiz
+		if nesne.position.x <= -600:
+			nesne.position.x = 1800
+
+	
