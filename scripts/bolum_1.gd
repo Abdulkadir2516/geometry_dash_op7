e45c758a36692d9b533b8eba728b3703c463628e
Abdulkadir Yeşilkaya <101483379+Abdulkadir2516@users.noreply.github.com>
2025-07-05 21:01:23 +0300

    zemin oluştu

--- scripts/bolum_1.gd
+++ scripts/bolum_1.gd
@@ -1,9 +1,12 @@
 extends Node2D
 
-@export var scroll_speed: float = 100.0  # Piksel/saniye
+@export var scroll_speed: float = 80.0  # Piksel/saniye
 var bg1: Sprite2D
 var bg2: Sprite2D
 var bg_width: float
+
+var g1: Sprite2D
+var g2: Sprite2D
 
 func _ready():
 	bg1 = $"Bg03-hd"
@@ -12,13 +15,28 @@ func _ready():
 
 	# İkinci arka planı hemen birincinin sağına koy
 	bg2.position.x = bg1.position.x + 1170
+	
+	g1 = $"Ground"
+	g2 = $"Ground2"
+
+	# İkinci arka planı hemen birincinin sağına koy
+	g2.position.x = g1.position.x + 1170
 
 func _process(delta):
 	bg1.position.x -= scroll_speed * delta
 	bg2.position.x -= scroll_speed * delta
 
+	g1.position.x -= scroll_speed * delta
+	g2.position.x -= scroll_speed * delta
+	
 	# Eğer bir arka plan tamamen ekranın soluna geçtiyse en sağa taşı
 	if bg1.position.x <= -600:
 		bg1.position.x = bg2.position.x + 1170
 	elif bg2.position.x <= -600:
 		bg2.position.x = bg1.position.x + 1170
+
+	# Eğer bir arka plan tamamen ekranın soluna geçtiyse en sağa taşı
+	if g1.position.x <= -600:
+		g1.position.x = g2.position.x + 1170
+	elif g2.position.x <= -600:
+		g2.position.x = g1.position.x + 1170
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,16 +1,19 @@
 extends CharacterBody2D
 
+var kontrol = true
 func _physics_process(delta):
 	
 	if Input.is_action_just_pressed("jump"):
-		
-		for i in range(10):
-			position.y = position.y-10
-			await get_tree().create_timer(0.02).timeout
 
-		await get_tree().create_timer(1).timeout
-		
-		
-		for i in range(10):
-			position.y = position.y + 10
-			await get_tree().create_timer(0.02).timeout
+		if kontrol:
+			kontrol = false
+			for i in range(20):
+				position.y = position.y-10
+				await get_tree().create_timer(0.02).timeout
+
+			for i in range(20):
+				position.y = position.y + 10
+				await get_tree().create_timer(0.02).timeout
+			
+			kontrol = true
+			
