c739e1d9d37f13fc8ba9d3da1849a12535a04526
Abdulkadir Yeşilkaya <101483379+Abdulkadir2516@users.noreply.github.com>
2025-07-12 21:27:29 +0300

    engel oluşturuldu

--- scripts/bolum_1.gd
+++ scripts/bolum_1.gd
@@ -1,15 +1,33 @@
 extends Node2D
 
-
 var bg_width: float
+var engel: CharacterBody2D
+var engel_isimleri: Array
+
+func _ready() -> void:
+	_bolum1_hazırlık()
 
 func _process(delta):
 	doit($"Bg03-hd", 2)
 	doit($"Bg03-hd2", 2)
-	doit($"Ground", 7)
-	doit($"Ground2", 7)
+	doit($"Ground", 5)
+	doit($"Ground2", 5)
+	doit(get_node("engel1").get_node("sprite"), 5)
 	
 func doit(nesne: Sprite2D, hiz: float):
 	nesne.position.x -= hiz
 	if nesne.position.x <= -600:
 		nesne.position.x = 1800
+
+func _bolum1_hazırlık():
+	for i in range(1,41):
+		engel = $engel.duplicate()
+		engel.name = "engel" + str(i)
+		engel.position = Vector2(i*600,520)
+		engel.visible = false
+		add_child(engel)
+	
+	$engel1.visible=true
+	$engel1.get_node("sprite").visible=true
+	$engel1.get_node("sprite2").visible=false
+	$engel1.get_node("sprite2").set_frame(3)
