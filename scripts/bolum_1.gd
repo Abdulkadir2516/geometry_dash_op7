88f87ba48ae88ab93a44a68d9ac040c3dfba746f
Abdulkadir Yeşilkaya <101483379+Abdulkadir2516@users.noreply.github.com>
2025-07-12 20:13:10 +0300

    .

--- scripts/bolum_1.gd
+++ scripts/bolum_1.gd
@@ -1,37 +1,15 @@
 extends Node2D
 
-@export var scroll_speed: float = 80.0  # Piksel/saniye
 
-var bg1: Sprite2D
-var bg2: Sprite2D
 var bg_width: float
 
-var g1: Sprite2D
-var g2: Sprite2D
-
-func _ready():
-	bg1 = $"Bg03-hd"
-	bg2 = $"Bg03-hd2"	
-	g1 = $"Ground"
-	g2 = $"Ground2"
-	
-	
-
 func _process(delta):
-	bg1.position.x -= scroll_speed * delta
-	bg2.position.x -= scroll_speed * delta
-
-	g1.position.x -= scroll_speed * delta *5
-	g2.position.x -= scroll_speed * delta *5
+	doit($"Bg03-hd", 2)
+	doit($"Bg03-hd2", 2)
+	doit($"Ground", 7)
+	doit($"Ground2", 7)
 	
-	doit(bg1)
-	doit(bg2)
-	doit(g1)
-	doit(g2)
-	
-func doit(nesne: Sprite2D):
-	
+func doit(nesne: Sprite2D, hiz: float):
+	nesne.position.x -= hiz
 	if nesne.position.x <= -600:
 		nesne.position.x = 1800
-
-	
