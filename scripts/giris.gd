4e9536497c00ea466d57c570d6b6374196498947
Abdulkadir Yeşilkaya <101483379+Abdulkadir2516@users.noreply.github.com>
2025-07-26 21:30:09 +0300

    giriş sayfasına başlandı

---
+++ scripts/giris.gd
@@ -0,0 +1,30 @@
+extends Control
+
+@onready var progress_bar = $ProgressBar
+
+func _ready():
+	$Sprite2D.visible=true
+	progress_bar.visible=true
+	$back.visible = false
+
+	
+	for i in range(20):
+		progress_bar.value += 5
+		await get_tree().create_timer(0.1).timeout
+		
+	$Sprite2D.visible=false
+	progress_bar.visible=false
+	
+	$back.visible=true
+
+
+func _on_button_pressed() -> void:
+	$back/Player.get_node("AnimatedSprite2D").frame += 1 
+
+
+func _on_button_2_pressed() -> void:
+	$back/Player.get_node("AnimatedSprite2D").frame -= 1
+
+
+func _on_baslat_pressed() -> void:
+	get_tree().change_scene_to_file("res://sahneler/Bolum_1.tscn")
