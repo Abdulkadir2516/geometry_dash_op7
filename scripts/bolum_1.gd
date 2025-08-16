bad0c9852b78e8b1b26883f19e08cd328a0a870a
Abdulkadir Yeşilkaya <101483379+Abdulkadir2516@users.noreply.github.com>
2025-08-16 21:30:55 +0300

    .

--- scripts/bolum_1.gd
+++ scripts/bolum_1.gd
@@ -14,12 +14,12 @@ func _ready() -> void:
 	$Player.get_node("AnimatedSprite2D").frame = Ortak.frame
 	$Player.get_node("ColorRect").color = Ortak.color
 
-	for i in range(1,10):
-		engel = $engel.duplicate()
-		engel.name = "engel" + str(i)
-		engel.position = Vector2(1200+ i*80,520)
-		engel.visible = true
-		add_child(engel)
+	#for i in range(1,10):
+		#engel = $engel.duplicate()
+		#engel.name = "engel" + str(i)
+		#engel.position = Vector2(1200+ i*80,520)
+		#engel.visible = true
+		#add_child(engel)
 
 func _process(delta):
 	doit($"Bg03-hd", 2)
@@ -27,11 +27,15 @@ func _process(delta):
 	doit($"Ground", 5)
 	doit($"Ground2", 5)
 	
-	for i in range(1,8):
-		doit2(get_node("engel"+str(i)), 4)
+	#for i in range(1,8):
+		#doit2(get_node("engel"+str(i)), 4)
 		
-	
-	
+	doit2(get_node("engel"), 4)
+		
+	doit2(get_node("cukur_engel"), 4)
+	doit2(get_node("sivri_engel"), 4)
+
+
 func doit(nesne: Sprite2D, hiz: float):
 	nesne.position.x -= hiz
 	if nesne.position.x <= -600:
--- scripts/player.gd
+++ scripts/player.gd
@@ -28,14 +28,18 @@ func _physics_process(delta):
 		
 		$CPUParticles2D.emitting=true
 
-	if position.y < 500 and not Input.is_action_just_pressed("jump") and kontrol:
+	if position.y < 515 and not Input.is_action_just_pressed("jump") and kontrol:
 		velocity.y += 2
 
 		move_and_collide(velocity * 7)
-		await get_tree().create_timer(.1).timeout
+		await get_tree().create_timer(.01).timeout
 			
 func dön():
 	for i in range(10):
 		rotation += 18
 		await get_tree().create_timer(0.1).timeout
  
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	print("Öldün")
